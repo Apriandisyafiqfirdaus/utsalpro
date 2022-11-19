a9c66a5a88db5811bf481ad71fd6e4036a2ce181
Apriandisyafiqfirdaus <friangamal@gmail.com>
2022-11-19 09:41:46 +0700

    menambah file

---
+++ variabel.java
@@ -0,0 +1,14 @@
+public class variabel {
+    public static void main(String[] args) {
+    String nama = "Apriandi syafiq firdaus";
+    int tahun = 1999;
+    System.out.println("Assalamu'alaikum");
+    System.out.println("Nama saya : " + nama);
+    System.out.println("Saya lahir ditahun : " + tahun);
+    }
+}
+    
+ 
+    
+
+  
